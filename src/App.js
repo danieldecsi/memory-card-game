e29eeba537f69ea7e7dfdfcd0c28735989e999e7
Daniel Decsi <daniel.decsi@supercharge.io>
2018-08-08 18:28:35 +0200

    Generate a random array of cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const DECK_SIZE = 6;
+
+    const deck = [
+      ...Array.from({ length: DECK_SIZE }).map((_, index) => index),
+      ...Array.from({ length: DECK_SIZE }).map((_, index) => index),
+    ];
+
+    for (let i = 0; i < deck.length; i++) {
+      const randomPosition = Math.floor(Math.random() * deck.length);
+      [deck[i], deck[randomPosition]] = [deck[randomPosition], deck[i]];
+    }
+
+    this.state = {
+      deck,
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        {this.state.deck.map((card, index) => (
+          <div
+            key={index}
+            style={{
+              display: 'inline-block',
+              padding: '1rem',
+              border: '1px solid #ccc',
+            }}
+          >
+            {card}
+          </div>
+        ))}
+      </Fragment>
     );
   }
 }
